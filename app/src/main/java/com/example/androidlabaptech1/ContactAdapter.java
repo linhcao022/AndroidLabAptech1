fbc057c16ab6df9e845949f6e363b36353ff2836
caolinh22 <48510866+caolinh22@users.noreply.github.com>
2021-09-01 02:02:11 +0800

    update lab 3.3

--- app/src/main/java/com/example/androidlabaptech1/ContactAdapter.java
+++ app/src/main/java/com/example/androidlabaptech1/ContactAdapter.java
@@ -1,22 +1,39 @@
 package com.example.androidlabaptech1;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
 
 import java.util.List;
 
 public class ContactAdapter extends BaseAdapter {
     private List<ContactModel> listContacts;
-    private Activity activity;
+    private Context mContext;
+    private IOnChildItemClick iOnChildItemClick;
 
-    public ContactAdapter(List<ContactModel> listContacts, Activity activity) {
+    public ContactAdapter(List<ContactModel> listContacts, Context mContext) {
         this.listContacts = listContacts;
-        this.activity = activity;
+        this.mContext = mContext;
+    }
+
+    public void registerChildItemClick(IOnChildItemClick iOnChildItemClick) {
+        this.iOnChildItemClick = iOnChildItemClick;
+    }
+
+    public void unRegisterChildItemClick() {
+        this.iOnChildItemClick = null;
     }
 
     @Override
@@ -36,27 +53,57 @@ public class ContactAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View convertView, ViewGroup viewGroup) {
-        if(convertView == null){
-            LayoutInflater inflater = activity.getLayoutInflater();
-            convertView = inflater.inflate(R.layout.item_contact, viewGroup, false);
+        View rowView = convertView;
+        // reuse views
+        if(rowView == null){
+            LayoutInflater inflater = ((Activity)mContext).getLayoutInflater();
+            rowView = inflater.inflate(R.layout.item_contact, null);
+
+            // configure view holder
             ViewHolder holder = new ViewHolder();
-            holder.tvName =(TextView) convertView.findViewById((R.id.tvName));
-            holder.tvPhone = (TextView) convertView.findViewById(R.id.tvPhone);
-            holder.ivAvatar =(ImageView) convertView.findViewById(R.id.ivAvatar);
-            convertView.setTag(holder);
+            holder.tvName =(TextView) rowView.findViewById((R.id.tvName));
+            holder.tvPhone = (TextView) rowView.findViewById(R.id.tvPhone);
+            holder.ivAvatar =(ImageView) rowView.findViewById(R.id.ivAvatar);
+            holder.btCall = (Button) rowView.findViewById(R.id.btCall);
+            holder.btEdit = (Button) rowView.findViewById(R.id.btEdit);
+            rowView.setTag(holder);
         }
 
+        // fill data
         ViewHolder holder = (ViewHolder) convertView.getTag();
-        ContactModel model = listContacts.get(i);
-        holder.tvName.setText(model.getName());
-        holder.tvPhone.setText(model.getPhone());
-        holder.ivAvatar.setImageResource(model.getImage());
+        holder.tvName.setText(listContacts.get(i).getName());
+        holder.tvPhone.setText(listContacts.get(i).getPhone());
+        holder.ivAvatar.setImageResource(listContacts.get(i).getImage());
+
+        holder.btCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCall(i);
+            }
+        });
+        holder.btEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iOnChildItemClick.onItemChildClick(i);
+            }
+        });
         return convertView;
+    }
+
+    private void onCall(int position){
+        ContactModel contact = listContacts.get(position);
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+contact.getPhone()));
+        if(ActivityCompat.checkSelfPermission(mContext, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+
+        }
+        mContext.startActivities(intent);
     }
 
     static class ViewHolder{
         TextView tvName;
         TextView tvPhone;
         ImageView ivAvatar;
+        Button btCall;
+        Button btEdit;
     }
 }
--- app/src/main/java/com/example/androidlabaptech1/MainActivity.java
+++ app/src/main/java/com/example/androidlabaptech1/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -13,27 +15,38 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView lvContact;
     private List<ContactModel> listContacts = new ArrayList<>();
+    private ListView lvContact;
+    private ContactAdapter mAdapter;
+    private ImageView ivUser;
+    private TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initData();
-        lvContact = (ListView) findViewById(R.id.lvContact);
-        ContactAdapter adapter = new ContactAdapter(listContacts, this);
-        lvContact.setAdapter(adapter);
+        initView();
+
+        mAdapter = new ContactAdapter(this, listContacts);
+        mAdapter.registerChildItemClick(this);
+        lvContact.setAdapter(mAdapter);
 
         lvContact.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 ContactModel contactModel = listContacts.get(i);
-                Toast.makeText(MainActivity.this, contactModel.getName(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, contactModel.getName()+ contactModel.getPhone(), Toast.LENGTH_SHORT).show();
             }
         });
 
 
+    }
+
+    private void initView() {
+        lvContact = (ListView) findViewById(R.id.lvContact);
+        ivUser = (ImageView) findViewById(R.id.ivUser);
+        tvName = (TextView) findViewById(R.id.tvName);
     }
 
     private void initData() {
@@ -46,5 +59,17 @@ public class MainActivity extends AppCompatActivity {
         contact = new ContactModel("Name4", "09812379823", R.drawable.img_3);
         listContacts.add(contact);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAdapter.unRegisterChildItemClick();
+    }
+
+    public void onItemChildClick(int position){
+        ContactModel contact = listContacts.get(position);
+        ivUser.setImageResource(contact.getImage());
+        tvName.setText(contact.getName());
     }
 }
